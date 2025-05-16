3a594034948821bfe9133b62d1fd979618df846e
Xavier DAVID <xav.david28@gmail.com>
2025-05-16 16:15:23 +0200

    Installation : Ajout d'une police personnalisée

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
